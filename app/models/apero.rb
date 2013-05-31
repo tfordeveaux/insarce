5036fb210bfa535668545f2245571b422a9b2d71
tfordeveaux <t.fordeveaux@gmai.com>
2013-05-31 17:10:04 +0200

    Vérification des champs + Redimensionnement image

--- app/models/apero.rb
+++ app/models/apero.rb
@@ -4,7 +4,10 @@ class Apero < ActiveRecord::Base
   belongs_to :category
   attr_accessible :category_id, :content, :date, :hour, :name, :user_id, :visibility
   
-  validate :visibility, :presence => true
+  validates :visibility, :presence => true
+  validates :name, :presence => true
+  validates :content, :presence => true
+  validates :category_id, :numericality => { :less_than_or_equal_to => 3}
   validate :check_date
  
 
